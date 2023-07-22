5f9ec08d669e7a829ab86a6d0e9ab09c4ab60efb
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-07-22 18:14:36 +0300

    Modify the bubble sort
    Make sure the algorithm is not doing unnecessary comparisons

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,22 +9,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
+	size_t i = 0, j;
 	int temp, swapped = 1;
 
 	while (swapped)
 	{
 		swapped = 0;
-		for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				swapped = 1;
 				print_array(array, size);
 			}
 		}
+		i++;
 	}
 }
