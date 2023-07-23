ee1784d144ed79a29ca73e16262b8706959e0da4
Harry Muriithi <hharryjjack@gmail.com>
2023-07-23 04:27:52 -0700

    task 1 error fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,11 +11,12 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0, j;
-	int temp, swapped = 1;
+	int temp;
+	bool swapped;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
+		swapped = false;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -23,11 +24,11 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = 1;
+				swapped = true;
 				print_array(array, size);
 			}
 		}
-		if (swapped == 0)
+		if (swapped == false)
 			break;
 	}
 }
